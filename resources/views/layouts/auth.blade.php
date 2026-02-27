8fd7199205f77de4ca4c63f511054e2c751a12af
Demba Faye <fayecodeur99@gmail.com>
2026-02-27 17:53:05 +0000

    Création du layout pour la connexion

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -12,7 +12,6 @@
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('auth/assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -21,10 +20,10 @@
     <link href="{{ asset('auth/assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
 </head>
 
-<body class="bg-gradient-primary">
+<body class="bg-gradient-primary d-flex align-items-center justify-content-center vh-100">
 
     <div class="container">
-        <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="card o-hidden border-0 shadow-lg">
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 @yield('content')
