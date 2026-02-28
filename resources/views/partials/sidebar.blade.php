b64c7b97329a923569593c3a619c017d205f1df8
Demba Faye <fayecodeur99@gmail.com>
2026-02-28 18:25:01 +0000

    Ajout du route login sur le sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -5,7 +5,7 @@
 
           <!-- Nav Item - Dashboard -->
           <li class="nav-item active">
-              <a class="nav-link" href="index.html">
+              <a class="nav-link" href="{{ route('dashboard') }}">
                   <i class="fas fa-fw fa-tachometer-alt"></i>
                   <span>@yield('title-section', 'Tableau de bord')</span></a>
           </li>
@@ -103,14 +103,4 @@
           <div class="text-center d-none d-md-inline">
               <button class="rounded-circle border-0" id="sidebarToggle"></button>
           </div>
-
-          <!-- Sidebar Message -->
-          <div class="sidebar-card d-none d-lg-flex">
-              <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
-              <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components,
-                  and more!</p>
-              <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to
-                  Pro!</a>
-          </div>
-
       </ul>
