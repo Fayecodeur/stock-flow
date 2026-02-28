17c7c8a06a78d78159e287cb80f8c9e760e74114
Demba Faye <fayecodeur99@gmail.com>
2026-02-28 17:26:54 +0000

    Décompositon du code

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>
+                Copyright &copy; {{ date('Y') }} —
+                <a href="https://dembafaye.com" target="_blank" class="text-primary font-weight-bold">
+                    Demba Faye
+                </a>
+            </span>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/scripts.blade.php
@@ -0,0 +1,9 @@
+<!-- Bootstrap core JavaScript-->
+<script src="{{ asset('auth/assets/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('auth/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="{{ asset('auth/assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{ asset('auth/assets/js/sb-admin-2.min.js') }}"></script>
